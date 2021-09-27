7c012218d032a864652348cda7cd1f67a331e944
hirokaz <hirokazu.ito.7@gmail.com>
2021-09-28 02:30:18 +0900

    chapter_14

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,7 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
